e473f668100a53c389b9c3fb5f08122ceb84163d
0sugo <josugo26@gmail.com>
2023-05-06 13:56:37 +0300

    Add prop values for author,title and genre

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,45 +1,12 @@
+import React from 'react';
 import BookItem from './BookItem';
 
 const Booklist = () => (
+  <>
+    <BookItem genre="Action" title="The Hunger Games" author="Suzanne Collins" />
+    <BookItem genre="Science Fiction" title="Dune" author="Frank Herbet" />
+  </>
 
-  <div className="lesson-panel">
-    <div>
-      <p>Action</p>
-      <h1>THE HUNGER GAMES</h1>
-      <p>Suzanne Collins</p>
-      <div className="meta">
-        <button type="button">Comments</button>
-        <button type="button">Remove</button>
-        <button type="button">Edit</button>
-      </div>
-
-    </div>
-    <div className="progress">
-      <svg className="progress-circle" xmlns="http://www.w3.org/2000/svg">
-        <circle
-          className="progress-circle-back"
-          cx="111"
-          cy="100"
-          r="44"
-        />
-        <circle
-          className="progress-circle-prog"
-          cx="111"
-          cy="100"
-          r="44"
-        />
-      </svg>
-      <div className="percentage-complete">
-        <h1 className="percent">64%</h1>
-        <p className="state">Completed</p>
-      </div>
-    </div>
-    <div className="currently">
-      <p>CURRENT CHAPTER</p>
-      <p>Chapter 17</p>
-      <button type="button">UPDATE PROGRESS</button>
-    </div>
-  </div>
 );
 
 export default Booklist;

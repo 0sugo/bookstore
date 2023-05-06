c04eefc2e9a076aeee5173e29cedcdf818333a77
0sugo <josugo26@gmail.com>
2023-05-06 13:57:51 +0300

    Restructure compunding element container to fragments

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,12 @@ import AddBook from './AddBook';
 import Booklist from './Booklist';
 
 const Home = () => (
-  <container>
+  <>
     <Booklist />
     <div className="Line" />
     <AddBook />
 
-  </container>
+  </>
 );
 
 export default Home;

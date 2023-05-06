101d5b64a659a401da5c19dcd3cd8fa791897dc7
0sugo <josugo26@gmail.com>
2023-05-06 16:30:08 +0300

    Add options component for the select

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,17 +1,11 @@
+import Options from './Options';
+
 const AddBook = () => (
   <div className="add-book">
     <h1>ADD NEW BOOK</h1>
     <form>
       <input type="text" placeholder="Book title" required />
-      <select placeholder="Category">
-        <option value="Action">Action</option>
-        <option value="Biography">Biography</option>
-        <option value="History">History</option>
-        <option value="Horror">Horror</option>
-        <option value="Kids">Kids</option>
-        <option value="Learning">Learning</option>
-        <option value="Sci-Fi">Sci-Fi</option>
-      </select>
+      <Options />
       <button type="submit" className="book-adder">ADD BOOK</button>
     </form>
   </div>
---
+++ src/components/Options.js
@@ -0,0 +1,13 @@
+const Options = () => (
+  <select placeholder="Category">
+    <option value="Action">Action</option>
+    <option value="Biography">Biography</option>
+    <option value="History">History</option>
+    <option value="Horror">Horror</option>
+    <option value="Kids">Kids</option>
+    <option value="Learning">Learning</option>
+    <option value="Sci-Fi">Sci-Fi</option>
+  </select>
+);
+
+export default Options;

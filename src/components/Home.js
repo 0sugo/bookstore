f55a02c702641174866cfed1271b3aeebfc58924
0sugo <josugo26@gmail.com>
2023-05-06 11:58:19 +0300

    Modify to accomodate new components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,12 @@
+import AddBook from './AddBook';
+import Booklist from './Booklist';
+
 const Home = () => (
   <container>
-    <div className="lesson-panel">
-      <div>
-        <p>Action</p>
-        <h1>THE HUNGER GAMES</h1>
-        <p>Suzanne Collins</p>
-        <div className="meta">
-          <button type="button">Comments</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
-        </div>
-
-      </div>
-      <div className="progress">
-        <svg className="progress-circle" xmlns="http://www.w3.org/2000/svg">
-          <circle
-            className="progress-circle-back"
-            cx="111"
-            cy="100"
-            r="44"
-          />
-          <circle
-            className="progress-circle-prog"
-            cx="111"
-            cy="100"
-            r="44"
-          />
-        </svg>
-        <div className="percentage-complete">
-          <h1 className="percent">64%</h1>
-          <p className="state">Completed</p>
-        </div>
-      </div>
-      <div className="currently">
-        <p>CURRENT CHAPTER</p>
-        <p>Chapter 17</p>
-        <button type="button">UPDATE PROGRESS</button>
-      </div>
-    </div>
+    <Booklist />
     <div className="Line" />
+    <AddBook />
 
-    <div className="add-book">
-      <h1>ADD NEW BOOK</h1>
-      <form>
-        <input type="text" placeholder="Book title" required />
-        <select placeholder="Category">
-          <option value="Action">Action</option>
-          <option value="Biography">Biography</option>
-          <option value="History">History</option>
-          <option value="Horror">Horror</option>
-          <option value="Kids">Kids</option>
-          <option value="Learning">Learning</option>
-          <option value="Sci-Fi">Sci-Fi</option>
-        </select>
-        <button type="submit" className="book-adder">ADD BOOK</button>
-      </form>
-    </div>
   </container>
 );
 

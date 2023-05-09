7902a9f1d4a0958ce526e4e3f11f7ee6c36f9308
0sugo <josugo26@gmail.com>
2023-05-09 18:34:09 +0300

    Add categoryslice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './books/bookSlice';
+import categoriesReducer from './cartegories/categories';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    category: categoriesReducer,
 
   },
 });

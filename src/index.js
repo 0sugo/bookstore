68d8a2cb8e6ef4efb5ed7a12a2ec1002f24f9d1b
0sugo <josugo26@gmail.com>
2023-05-09 15:06:14 +0300

    Fix destructuring on import and organise diles to the expected file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider>
-     <App />
-    </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );

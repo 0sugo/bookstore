aa4392fb34fbc8f524366e7e2ed1d6197dc74820
0sugo <josugo26@gmail.com>
2023-05-10 14:50:32 +0300

    Add use selector to get fetch and displayvalues from store

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
-const Booklist = () => (
-  <>
-    <BookItem genre="Action" title="The Hunger Games" author="Suzanne Collins" />
-    <BookItem genre="Science Fiction" title="Dune" author="Frank Herbet" />
-  </>
-
-);
-
+const Booklist = () => {
+  const { books } = useSelector((store) => store.counter);
+  return (
+    <>
+      { books.map((item) => (
+        <BookItem
+          key={item.item_id}
+          genre={item.category}
+          title={item.title}
+          author={item.author}
+        />
+      ))}
+    </>
+  );
+};
 export default Booklist;

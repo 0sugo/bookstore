9254625c114b822b6ff081dc91421860af3d3420
0sugo <josugo26@gmail.com>
2023-05-11 10:18:52 +0300

    Add dispatch to remove specific item

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import BookItem from './BookItem';
+import { removeBook } from '../redux/books/bookSlice';
 
 const Booklist = () => {
   const { books } = useSelector((store) => store.counter);
+  const dispatch = useDispatch();
+
+  const handleRemove = (e) => {
+    dispatch(removeBook(e.target.id));
+  };
   return (
     <>
-      { books.map((item) => (
+      {books.map((item) => (
         <BookItem
           key={item.item_id}
           genre={item.category}
           title={item.title}
           author={item.author}
+          removeBook={handleRemove}
+          itemId={item.item_id}
         />
       ))}
     </>

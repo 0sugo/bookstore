6f6fbb7a8c27f0a38ab0dc4dc0a99c663a69c81d
0sugo <josugo26@gmail.com>
2023-05-11 09:10:47 +0300

    Add Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
-const Button = () => (
+import { PropTypes } from 'prop-types';
+
+const Button = ({ text, purpose, id }) => (
   <div>
-    <Button>Add</Button>
+    <button type="submit" className="book-adder" id={id} onClick={(e) => { purpose(e); }}>
+      {' '}
+      {text}
+    </button>
   </div>
 );
-
+Button.propTypes = { text: PropTypes.string.isRequired };
+Button.propTypes = { purpose: PropTypes.func.isRequired };
+Button.propTypes = { id: PropTypes.string.isRequired };
 export default Button;
